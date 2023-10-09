4702def2f068454d9130354139bd0a5651348fd9
Mike <mike.munene@student.moringaschool.com>
2023-10-09 08:57:45 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, potential_anagrams):
+        sorted_word = sorted(self.word)
+
+        matches = []
+
+        for possible in potential_anagrams:
+            sorted_possible = sorted(possible.lower())
+
+            if sorted_possible == sorted_word and possible.lower() != self.word:
+                matches.append(possible)
+
+        return matches
+
+
+listen = Anagram("listen")
+matches = listen.match(["enlists", "google", "inlets", "banana"])
+print(matches)
